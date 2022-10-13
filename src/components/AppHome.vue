c20828adb1497a16d83a526b810f73cf38dca897
Rutuja Santikar <santikarrutuja@gmail.com>
2022-10-13 21:01:30 +0530

    components added

---
+++ src/components/AppHome.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <div class="home-slider">
+      </div>
+    <button>Get Started</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'AppHome',
+ 
+}
+</script>
+
+<style scoped>
+
+</style>
