45d4af289014329311bca3594495bf4affdb64ba
Rutuja Santikar <santikarrutuja@gmail.com>
2022-10-14 22:18:13 +0530

    home page ui added

--- src/components/AppHome.vue
+++ src/components/AppHome.vue
@@ -1,18 +1,47 @@
 <template>
-  <div>
-    <div class="home-slider">
-      </div>
-    <button>Get Started</button>
+  <div class="body">
+    <header class="heading-title">
+      <h1>Enjoy Cooking</h1>
+      <h4>Delicious and detailed restaurant recipes !!</h4>
+    </header>
+    <section class="img-cont">
+      <img v-bind:src="slider1" class="img-style"/>
+    </section>
+    <div class="start-btn">Get Started</div>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'AppHome',
- 
+name:"AppHome",
+data(){
+  return{
+  slider1:require("../assets/slider2.png")
+}
+
+}
 }
 </script>
 
-<style scoped>
+<style scoped> 
+.body{
+  background-color: white;
+}
+.heading-title{
+  color:   #f65b4a;
+  font-family:'Times New Roman', Times, serif;
+  
+}
+.img-cont, .start-btn{
+  display: flex;
+  justify-content: center;
+}
+.start-btn{
+  background-color: #f65b4a;
+  color: white;
+  padding: 20px;
+  
+
+}
 
 </style>
--- src/main.js
+++ src/main.js
@@ -1,6 +1,11 @@
 import Vue from 'vue'
 import App from './App.vue'
+import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
 
+Vue.use(BootstrapVue);
+Vue.use(IconsPlugin);
+import "bootstrap/dist/css/bootstrap.min.css"
+import "bootstrap-vue/dist/bootstrap-vue.css"
 Vue.config.productionTip = false
 
 new Vue({
