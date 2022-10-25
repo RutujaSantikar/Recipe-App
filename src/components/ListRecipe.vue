d65b42a5d22955e7c78c286fa0230b7373943bae
Rutuja Santikar <santikarrutuja@gmail.com>
2022-10-25 10:56:23 +0530

    search bar & recipe container added

--- src/components/ListRecipe.vue
+++ src/components/ListRecipe.vue
@@ -1,5 +1,23 @@
 <template>
-  <h1>list of recipe works</h1>
+  <div>
+    <div>
+  <b-card
+    title="Card Title"
+    img-src="https://picsum.photos/600/300/?image=25"
+    img-alt="Image"
+    img-top
+    tag="article"
+    style="max-width: 19rem ;"
+    class="mb-2"
+  >
+    <b-card-text>
+      Some quick example text to build on the card title and make up the bulk of the card's content.
+    </b-card-text>
+
+    <b-button href="#" variant="primary">Go somewhere</b-button>
+  </b-card>
+</div>
+  </div>
 </template>
 
 <script>
--- src/components/SearchRecipe.vue
+++ src/components/SearchRecipe.vue
@@ -1,6 +1,10 @@
 <template>
-  <div>
-    <div class="search-cont">
+  <div class="body">
+    
+    <div class="heading-style">
+    <h1>What do you want to cook today ?</h1>
+    
+    
       <input type="search" placeholder="search for recipe..." class="search-bar"/>
 
     </div>
@@ -9,13 +13,39 @@
 
 <script>
 export default {
-name:"SearchRecipe"
+name:"SearchRecipe",
+
 }
 </script>
 
 <style scoped>
-h1{
-    color:pink;
+.body{
+ 
+  font-family:'Times New Roman', Times, serif;
+  display: flex;
+  justify-content: center;
+  align-items: center;
 }
 
+h1{
+  padding:10px;
+}
+.search-bar{
+  background-color:rgba(218, 216, 216, 0.625);
+  outline: none;
+  border: 1px solid lightgray;
+  border-radius: 15px;
+  padding: 10px;
+  width: 100%;
+
+}
+@media (max-width:600px) {
+
+  .search-bar{
+    padding:9px;
+    width:90%;
+    margin-left: 18px;
+  }
+   
+}
 </style>
